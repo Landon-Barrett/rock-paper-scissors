493374035230ca0eb7cf4a6ff3e349a5d4ff28be
Landon Barrett <lbarrett@stu.jsu.edu>
2024-01-25 14:30:25 -0600

    OnClick Listeners added for buttons.

--- app/src/main/java/com/example/cs408lab1brockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/cs408lab1brockpaperscissors/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.cs408lab1brockpaperscissors.databinding.ActivityMainBinding;
 
@@ -15,6 +16,33 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.rockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView t = binding.output;
+                t.setText("test1");
+            }
+        });
+
+        binding.paperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView t = binding.output;
+                t.setText("test2");
+            }
+        });
+
+        binding.scissorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TextView t = binding.output;
+                t.setText("test4");
+            }
+        });
     }
 
 }
